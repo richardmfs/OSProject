e845685f21a5a69d1488120524ebf992e74b05d0
Jadetree001 <Jadetree001@users.noreply.github.com>
2024-11-12 11:54:37 -0500

    update

---
+++ main.py
@@ -0,0 +1,6 @@
+from shell import Shell
+
+# Entry point of the program
+if __name__ == "__main__":
+    shell = Shell()  # Create an instance of the Shell class
+    shell.start()    # Start the shell interface
